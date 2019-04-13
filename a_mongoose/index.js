b98398add6be1dcab5ab86ba4643fa1f1a65ede7
Dmitry <dimka11lp@gmail.com>
2019-04-13 23:17:03 +0600

    playing with promise and async

--- a_mongoose/index.js
+++ a_mongoose/index.js
@@ -10,10 +10,10 @@ var accDataSchema = mongoose.Schema({
 
 var accDataModel = mongoose.model("AccData", accDataSchema);
 let time_delta = 0
-    /**
-     * @type {Array}
-     */
-    let requests_array = [];
+/**
+ * @type {Array}
+ */
+let requests_array = [];
 
 mongoose.connect('mongodb://localhost/AccData', { useNewUrlParser: true }, (err) => {
 
@@ -23,18 +23,18 @@ mongoose.connect('mongodb://localhost/AccData', { useNewUrlParser: true }, (err)
     request.on('data', function (data) {
       //console.log(JSON.parse(data));
       let json_object = JSON.parse(data);
-      requests_array.push(json_object + "\n")
+      requests_array.push(json_object)
       timeDelta(json_object.timestamp);
       let newAccData = new accDataModel({
         _id: new mongoose.Types.ObjectId(),
         timestamp: json_object.timestamp,
         sensors: json_object.sensors
       });
-      newAccData.save((err)=>{
+      newAccData.save((err) => {
         if (err) {
           console.log('error' + err);
           return;
-        } 
+        }
         //console.log('successfully saved');
       });
 
@@ -56,15 +56,51 @@ mongoose.connect('mongodb://localhost/AccData', { useNewUrlParser: true }, (err)
 /**
  * write to disk with 1 second interval
  */
-(function repeatedConsoleLogger(){
-  setInterval(()=> {
+(function repeatedConsoleLogger() {
+  setInterval(() => {
     console.log(requests_array.length);
-    fs.appendFile("test.json", JSON.stringify(requests_array),(err)=> {
+    fs.appendFile("test.json", JSON.stringify(requests_array), (err) => {
       if (err) console.log(err);
       requests_array = []
     })
   }, 1000)
+});
+
+(function repeatedConsoleLoggerPromise() {
+  const util = require('util');
+  fs.appendFile = util.promisify(fs.appendFile);
+  setInterval(() => {
+    console.log(requests_array.length);
+    fs.appendFile("test.json", JSON.stringify(requests_array))
+      .then(() => {
+        requests_array = [];
+      })
+      .catch((err) => { console.log(err) })
+  }, 1000)
+});
+
+(function repeatedConsoleLoggerAsync() {
+  const util = require('util');
+  fs.appendFile = util.promisify(fs.appendFile);
+   setInterval(async () => {
+    console.log(requests_array.length);
+    let err = await fs.appendFile("test.json", JSON.stringify(requests_array));
+    if (err) console.log(err);
+    requests_array = [];
+  }, 1000)
 })();
+
+/*
+const fsPromises = fs.promises;
+async function listDir() {
+  try {
+    return await fsPromises.readdir('path/to/dir');
+  } catch (err) {
+    console.error('Error occured while reading directory!', err);
+  }
+}
+listDir();
+*/
 
 
 process.on('SIGINT', function () {
