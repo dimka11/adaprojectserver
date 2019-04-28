9a946228e7b701f11709311d99e69e48dd47aa21
Dmitry <dimka11lp@gmail.com>
2019-04-28 13:51:10 +0600

    add upload files based on formidable

--- MainServer/index.js
+++ MainServer/index.js
@@ -1,6 +1,7 @@
 const handler = require('serve-handler');
 const http = require('http');
 const fs = require('fs');
+var formidable = require('formidable');
 
 var dateToStr = require('./dateToStr');
 var jsonToStr = require('./jsonToStr')
@@ -9,6 +10,7 @@ var jsonToStr = require('./jsonToStr')
 
 let realTimeData;
 let PORT = 8080;
+let uploadedFilesFolder = 'C:/VSI/';
 
 let filename = "data/" + "acc_data_" + dateToStr.getStringDate() + ".csv";
 
@@ -16,57 +18,86 @@ let filename = "data/" + "acc_data_" + dateToStr.getStringDate() + ".csv";
 fs.open(filename, "wx", function (err, fd) {
   // handle error
   fs.close(fd, function (err) {
-      // handle error
+    // handle error
   });
 });
 
-const server = http.createServer((request, response) => {
-  console.log('request was sent ' + request.url);
-  if (request.url === '/putDataCSV') {
-    request.on('data', function (data) {
+http.createServer((req, res) => {
+  console.log('req was sent ' + req.url);
+
+  if (req.url == '/fileupload') {
+    console.log(req.rawHeaders);
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      if (err) throw err;
+      var oldpath = files.file.path;
+      var newpath = 'C:/VSI/' + files.file.name;
+      fs.rename(oldpath, newpath, function (err) {
+        //if (err) throw err;
+        //res.write('File uploaded and moved!');
+        if (err) {
+          res.write(JSON.stringify({
+            success: false,
+            message: 'error happend'
+          })
+          );
+          res.end()
+          throw err;
+        }
+
+        res.write(JSON.stringify({
+          success: true,
+          message: 'Successfully Uploaded'
+        })
+        );
+        res.end();
+      });
+    }).on('error', () => 'error happend');
+  }
+
+  else if (req.url === '/putDataCSV') {
+    req.on('data', function (data) {
       //console.log(JSON.parse(data))
-      
+
       let json_object = JSON.parse(data)
       let stringToWrite = jsonToStr(json_object, filename);
-  
+
       fs.appendFileSync(filename, stringToWrite, function (err) {
         if (err) {
           return console.log(err);
         }
       });
     });
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end('Hello Node.js Server!')
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end('Hello Node.js Server!')
   }
 
-  if (request.url === '/putData') {
+  else if (req.url === '/putData') {
     // work with data from client
-    request.on('data', (data) => {
+    req.on('data', (data) => {
       realTimeData = JSON.parse(data);
     })
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("");
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("");
   }
 
-  else if (request.url === '/getRealTimeData') {
+  else if (req.url === '/getRealTimeData') {
     // send data to frontend
-    response.writeHead(200, { "Content-Type": "application/json" });
+    res.writeHead(200, { "Content-Type": "application/json" });
     let json = JSON.stringify(realTimeData);
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end(json);
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end(json);
   }
   else {
-    return handler(request, response,
+    return handler(req, res,
       {
         "public": "./static",
         "symlinks": true
       }).then(() => console.log(""));
   }
-});
-
-server.listen(PORT, () => {
+}).listen(PORT, () => {
   console.log('Running at http://localhost:' + PORT);
-});
+});;
 
 process.on('uncaughtException', function (err) {
   // This should not happen
